36650c3e3eec27212c1789deb6117f540fdf85d2
shizhx <main.shizhi@gmail.com>
2026-01-08 11:31:12 +0800

    feat: 完善框架

--- main.go
+++ main.go
@@ -5,25 +5,58 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"sort"
 	"strings"
 )
 
+type Args struct {
+	Listen   string
+	Connect  string
+	CertPath string
+	KeyPath  string
+	CAPath   string
+}
+
+// Stream a bidi stream
+type Stream interface {
+	io.ReadWriteCloser
+	CloseRead() error
+	CloseWrite() error
+}
+
+// ServerSession a session
+type ServerSession interface {
+	AcceptStream() (Stream, error)
+}
+
+type ClientSession interface {
+	OpenStream() (Stream, error)
+}
+
 func main() {
-	listen := flag.String("listen", "tcp://127.0.0.1:1443", "Listen address, available schemes: unix://, tcp://, tls://, grpc://, capnp://")
-	connect := flag.String("connect", "tcp://127.0.0.1:", "Connect address, available schemes: unix://, tcp://, tls://, grpc://, capnp://")
+	listen := flag.String("listen", "tcp://127.0.0.1:1443", fmt.Sprintf("Listen address, available schemes: %s", getAvailableListenSchemes()))
+	connect := flag.String("connect", "tcp://127.0.0.1:", fmt.Sprintf("Connect address, available schemes: %s", getAvailableConnectSchemes()))
 	certPath := flag.String("cert", "./server.crt", "Cert path for listening")
 	keyPath := flag.String("key", "./server.key", "Key path for listening")
 	caPath := flag.String("ca", "./ca.crt", "CA cert path for connecting")
-
 	flag.Parse()
+
 	fmt.Println("Listen:", *listen)
 	fmt.Println("Connect:", *connect)
 	fmt.Println("Cert:", *certPath)
 	fmt.Println("Key:", *keyPath)
 	fmt.Println("CA:", *caPath)
 
-	server := newServerSession(*listen, *certPath, *keyPath)
-	client := newClientSession(*connect, *caPath)
+	args := Args{
+		Listen:   *listen,
+		Connect:  *connect,
+		CertPath: *certPath,
+		KeyPath:  *keyPath,
+		CAPath:   *caPath,
+	}
+
+	server := newServerSession(args)
+	client := newClientSession(args)
 
 	for {
 		down, err := server.AcceptStream()
@@ -77,12 +110,62 @@ func copyStream(src, dst Stream, srcName, dstName string) (written int64, copyEr
 	return
 }
 
-func newServerSession(listen, certPart, keyPath string) ServerSession {
-	return nil
+type ServerSessionCreator func(args Args) (ServerSession, error)
+
+var serverSessionCreators map[string]ServerSessionCreator = make(map[string]ServerSessionCreator)
+
+func newServerSession(args Args) ServerSession {
+	scheme, _ := splitSchemeAddr(args.Listen)
+	creator, ok := serverSessionCreators[scheme]
+	if !ok || creator == nil {
+		log.Fatalf("Unknown listen scheme: %s", scheme)
+	}
+
+	session, err := creator(args)
+	if err != nil {
+		log.Fatalf("Failed to create server session: %v", err)
+	}
+
+	return session
 }
 
-func newClientSession(connect, caPath string) ClientSession {
-	return nil
+func getAvailableListenSchemes() string {
+	var schemes []string
+	for scheme := range serverSessionCreators {
+		schemes = append(schemes, scheme)
+	}
+
+	sort.Strings(schemes)
+	return strings.Join(schemes, ", ")
+}
+
+type ClientSessionCreator func(args Args) (ClientSession, error)
+
+var clientSessionCreators map[string]ClientSessionCreator = make(map[string]ClientSessionCreator)
+
+func newClientSession(args Args) ClientSession {
+	scheme, _ := splitSchemeAddr(args.Connect)
+	creator, ok := clientSessionCreators[scheme]
+	if !ok || creator == nil {
+		log.Fatalf("Unknown connect scheme: %s", scheme)
+	}
+
+	session, err := creator(args)
+	if err != nil {
+		log.Fatalf("Failed to create client session: %v", err)
+	}
+
+	return session
+}
+
+func getAvailableConnectSchemes() string {
+	var schemes []string
+	for scheme := range clientSessionCreators {
+		schemes = append(schemes, scheme)
+	}
+
+	sort.Strings(schemes)
+	return strings.Join(schemes, ", ")
 }
 
 func splitSchemeAddr(s string) (scheme, addr string) {
@@ -92,20 +175,4 @@ func splitSchemeAddr(s string) (scheme, addr string) {
 	}
 
 	return parts[0], parts[1]
-}
-
-// Stream a bidi stream
-type Stream interface {
-	io.ReadWriteCloser
-	CloseRead() error
-	CloseWrite() error
-}
-
-// ServerSession a session
-type ServerSession interface {
-	AcceptStream() (Stream, error)
-}
-
-type ClientSession interface {
-	OpenStream() (Stream, error)
 }
